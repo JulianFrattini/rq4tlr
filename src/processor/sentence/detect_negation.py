6e4ffcbeb309e5132033a150e8ba119153de027b
Tobias Hey <hey@kit.edu>
2025-01-31 09:37:11 +0100

    add a simple heuristic to detect negated verbs as well

--- src/processor/sentence/detect_negation.py
+++ src/processor/sentence/detect_negation.py
@@ -13,4 +13,8 @@ class DetectNegation(SentenceProcessor):
         for e in sentence.doc.ents:
             if e._.negex:
                 return True
+        for token in sentence.doc:
+            if token.text in ['not', "n't", "wouldn't", 'never', 'nowhere', 'noone', 'no-one']:
+                if token.dep_ == 'neg' and (token.head.pos_ == 'VERB' or token.head.pos_ == 'AUX'):
+                    return True
         return False
