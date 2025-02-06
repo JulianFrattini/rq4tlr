23c06b799c1114e0a3f2dbb2af57ccae77a27cce
Julian Frattini <julian.frattini@alice-dsl.net>
2025-02-06 12:00:24 +0100

    Improved visualization of the data

--- src/util/static.py
+++ src/util/static.py
@@ -2,8 +2,12 @@
 PATH_RAW_INPUT: str = '../data/input/raw/'
 PATH_RAW_SUPPLEMENTARY: str = '../data/input/supplementary/'
 PATH_RAW_OUTPUT: str = '../data/input/formatted/'
-PATH_OUTPUT: str = '../data/output/'
 PATH_RAW_GOLDSTANDARDS: str = '../data/input/raw/TLR_goldstandards/'
+
 PATH_LABELING: str = '../data/labeling/'
+
+PATH_OUTPUT: str = '../data/output/'
+PATH_FIGURES: str = '../figures/'
+PATH_FIGURES_DISTRIBUTIONS: str = '../figures/distributions/'
 
 LEVELS: list[str] = ['usecase', 'subflow', 'sentence']
